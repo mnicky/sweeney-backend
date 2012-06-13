b9f469b1578f11afb41841bbb960dbfb8cb0563b
mnicky <xmnicky@gmail.com>
2012-06-13 19:31:16 +0200

    add Korma, PgSQL and Feedparser dependencies

--- project.clj
+++ project.clj
@@ -5,7 +5,9 @@
   :dependencies [[org.clojure/clojure "1.4.0"]
                    [org.clojure/core.incubator "0.1.0"]
                    [overtone/at-at "1.0.0"]
-                   [org.clojars.scsibug/feedparser-clj "0.4.0"]]
+                   [org.clojars.scsibug/feedparser-clj "0.4.0"]
+                   [postgresql "9.1-901-1.jdbc4"]
+                   [korma "0.3.0-beta10"]]
   :java-source-paths ["src/java"]
   :source-paths ["src/clojure"]
   :test-paths ["test/clojure"])
