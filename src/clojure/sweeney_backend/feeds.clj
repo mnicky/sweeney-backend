3fec9747670aab2da5e1dacfded5e99a4253f1f7
mnicky <xmnicky@gmail.com>
2012-06-21 17:33:06 +0200

    use clojure.jdbc instead of korma + underscore as separator in 'Story' keys

--- src/clojure/sweeney_backend/feeds.clj
+++ src/clojure/sweeney_backend/feeds.clj
@@ -1,31 +1,29 @@
 (ns sweeney-backend.feeds
   (:require [sweeney-backend.utils :as utils])
   (:require [feedparser-clj.core :as parser])
-  (:require [korma.core :refer :all]))
+  (:require [clojure.java.jdbc :as jdbc]))
 
-(defrecord Story [feed-type title url description published-at])
+(defrecord Story [feed_type title url description published_at])
 
 (defn save-story
-  "Add the story to the database with specified feed-id."
-  [story feed-id]
-  (let [{:keys [feed-type title url description published-at]} story]
-    (insert :stories (values {:feed_id feed-id
-                              :feed_type feed-type
-                              :title title
-                              :url url
-                              :description description
-                              :published_at published-at}))))
+  "Add the story to the database with specified feed-id using db-connection."
+  [db-connection story feed-id]
+    (jdbc/with-connection
+      db-connection
+      (jdbc/transaction
+        (jdbc/insert-record :stories (assoc story :feed_id feed-id)))))
 
 (defn parse-feed
-  "Parses the feed at the given url and returns lazy seq of its entries."
+  "Parses the feed at the given url and returns lazy seq of its entries
+  as instances of Story."
   [url]
   (let [feed (parser/parse-feed url)
         {:keys [entries published-date]} feed]
     (for [e entries]
       (let [feed-published (or published-date (utils/now))
             {:keys [title link description contents published-date updated-date]} e]
-        (Story/create {:feed-type "RssFeed"
+        (Story/create {:feed_type "RssFeed"
                        :title title
                        :url link
                        :description (or (:value (first contents)) (:value description) "")
-                       :published-at (utils/to-sql-date (or updated-date published-date feed-published))})))))
+                       :published_at (utils/to-sql-date (or updated-date published-date feed-published))})))))
