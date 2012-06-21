6aeb7ea32455e5a7747377602edfaaee35a3c694
mnicky <xmnicky@gmail.com>
2012-06-21 15:33:46 +0200

    add 'c3p0' library to the dependencies

--- project.clj
+++ project.clj
@@ -7,8 +7,10 @@
                    [org.clojure/core.incubator "0.1.0"]
                    [overtone/at-at "1.0.0"]
                    [org.clojars.scsibug/feedparser-clj "0.4.0"]
-                   [postgresql "9.1-901-1.jdbc4"]
-                   [korma "0.3.0-beta11"]]
+                   [korma "0.3.0-beta11"]
+                   [c3p0/c3p0 "0.9.1.2"]
+                   ;[com.jolbox/bonecp "0.7.1.RELEASE"]
+                   [postgresql "9.1-901-1.jdbc4"]]
   :profiles {:dev {:dependencies [[org.xerial/sqlite-jdbc "3.7.2"]]
                      ;:warn-on-reflection true
                      }}
