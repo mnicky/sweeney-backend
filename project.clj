e5135763e3db66a88df84fe4b6974518767bf75b
mnicky <xmnicky@gmail.com>
2012-06-22 15:34:51 +0200

    add feed tests

--- project.clj
+++ project.clj
@@ -7,9 +7,8 @@
                    [org.clojure/core.incubator "0.1.0"]
                    [overtone/at-at "1.0.0"]
                    [org.clojars.scsibug/feedparser-clj "0.4.0"]
-                   [korma "0.3.0-beta11"]
+                   [org.clojure/java.jdbc "0.2.3"]
                    [c3p0/c3p0 "0.9.1.2"]
-                   ;[com.jolbox/bonecp "0.7.1.RELEASE"]
                    [postgresql "9.1-901-1.jdbc4"]]
   :profiles {:dev {:dependencies [[org.xerial/sqlite-jdbc "3.7.2"]]
                      ;:warn-on-reflection true
--- test/clojure/sweeney_backend/feeds_test.clj
+++ test/clojure/sweeney_backend/feeds_test.clj
@@ -1,9 +1,34 @@
 (ns sweeney-backend.feeds-test
   (:require [clojure.java.io :as io])
+  (:require [clojure.java.jdbc :as jdbc])
   (:use clojure.test
-        sweeney-backend.feeds))
+        sweeney-backend.feeds
+        sweeney-backend.dbpool)
+  (:import sweeney_backend.feeds.Story))
 
 (def rss-file "./test/resources/test-rss1.xml")
+
+(def test-db {:classname "org.sqlite.JDBC"
+              :subprotocol "sqlite"
+              :subname ":memory:"
+              :user "test"
+              :password "test"})
+
+(def test-db-pool (delay (db-pool test-db)))
+
+(defn create-test-tables
+  [db-conn]
+  (jdbc/with-connection db-conn
+    (jdbc/create-table :stories
+                       [:id :integer]
+                       [:feed_id :integer]
+                       [:feed_type :text]
+                       [:title :text]
+                       [:url :text]
+                       [:description :text]
+                       [:published_at :date])))
+
+(use-fixtures :once (fn [x] (create-test-tables @test-db-pool) (x)))
 
 (deftest parse-feed-test
   (let [feed (io/file rss-file)]
@@ -14,8 +39,23 @@
           article (first parsed)]
       (is (= 25 (count parsed)))
       (is (= 5 (count article)))
-      (is (= (:feed-type article) "RssFeed"))
+      (is (= (:feed_type article) "RssFeed"))
       (is (= (:title article) "Opera zvyšuje stabilitu, oddělila pluginy do vlastních procesů"))
       (is (= (:url article) "http://www.root.cz/zpravicky/opera-zvysuje-stabilitu-oddelila-pluginy-do-vlastnich-procesu/#utm_source=rss&utm_medium=text&utm_campaign=rss"))
       (is (= (:description article) "Vyšla nová verze internetového prohlížeče Opera 12. Kromě nové podpory snadné změny vzhledu je zde i množství technických věcí, které nejsou na první pohled vidět. Například rychlejší start i s množstvím panelů, oddělení pluginů do samostatných..."))
-      (is (= (:published-at article) #inst "2012-06-15T14:53:13.000-00:00")))))
+      (is (= (:published_at article) #inst "2012-06-15T14:53:13.000-00:00")))))
+
+(deftest save-story-test
+  (let [story (Story. "test-feed-type" "test-feed-title" "test-feed-url" "test-feed-description" #inst "2012-01-01T12:00")]
+    (save-story @test-db-pool story 12)
+    (jdbc/with-connection @test-db-pool
+      (jdbc/with-query-results res
+        ["SELECT * FROM stories LIMIT 1"]
+        (is (= (first res)
+               {:id nil
+                :feed_id 12
+                :feed_type "test-feed-type"
+                :title "test-feed-title"
+                :url "test-feed-url"
+                :description "test-feed-description"
+                :published_at (.getTime #inst "2012-01-01T12:00")} ))))))
