48e44f590d3f9c73dbd68a0f6300ff7d21b061f5
mnicky <xmnicky@gmail.com>
2012-06-26 00:26:59 +0200

    add 'find-story-by-url' + 'parse-feed' return also feed info

--- src/clojure/sweeney_backend/feeds.clj
+++ src/clojure/sweeney_backend/feeds.clj
@@ -21,25 +21,40 @@
         (jdbc/insert-record :rss_feeds feed))))
 
 (defn parse-feed
-  "Parses the feed at the given `url` and returns lazy seq of its entries
-  as instances of Story."
+  "Parses the feed at the given `url` and returns map with this format:
+    :info - information about the feed as instance of Feed
+    :stories - lazy seq of feed's entries as instances of Story"
   [url]
   (let [feed (parser/parse-feed url)
-        {:keys [entries published-date]} feed]
-    (for [e entries]
-      (let [feed-published (or published-date (utils/now))
-            {:keys [title link description contents published-date updated-date]} e]
-        (Story/create {:feed_type "RssFeed"
-                       :title title
-                       :url link
-                       :description (or (:value (first contents)) (:value description) "")
-                       :published_at (utils/to-sql-date (or updated-date published-date feed-published))})))))
+        {:keys [entries image link published-date title uri]} feed
+        info (Feed/create {:url url
+                           :title title
+                           :link (or link uri)
+                           :image (:url image)})]
+    {:stories (for [e entries]
+                (let [feed-published (or published-date (utils/now))
+                      {:keys [title link description contents published-date updated-date]} e]
+                  (Story/create {:feed_type "RssFeed"
+                                 :title title
+                                 :url link
+                                 :description (or (:value (first contents)) (:value description) "")
+                                 :published_at (utils/to-sql-date (or updated-date published-date feed-published))})))
+     :info info}))
 
 (defn find-feed-by-url
-  "Returns map of columns of rss_feeds table with given `url` using given
-  `db-connection`. If no rss feed with that `url` exists, returns nil."
+  "Returns map of columns of `rss_feeds` table with given `url` using given
+  `db-connection`. If no rss feed with given `url` exists, returns nil."
   [db-connection url]
   (jdbc/with-connection db-connection
     (jdbc/with-query-results res
       ["SELECT id, url, title, link, image FROM rss_feeds WHERE url=? LIMIT 1" url]
+      (first res))))
+
+(defn find-story-by-url
+  "Returns map of columns of `stories` table with given `url` using given
+  `db-connection`. If no story with given `url` exists, returns nil."
+  [db-connection url]
+  (jdbc/with-connection db-connection
+    (jdbc/with-query-results res
+      ["SELECT id, feed_id, feed_type, title, url, description, published_at FROM stories WHERE url=? LIMIT 1" url]
       (first res))))
--- test/clojure/sweeney_backend/feeds_test.clj
+++ test/clojure/sweeney_backend/feeds_test.clj
@@ -49,20 +49,30 @@
                   (finally
                     (drop-test-tables @test-db-pool)))))
 
-(deftest parse-feed-test
+(deftest parse-feed-stories-test
   (let [feed (io/file rss-file)]
     (is (instance? java.io.File feed))
     (is (.exists feed))
 
-    (let [parsed (parse-feed feed)
-          article (first parsed)]
-      (is (= 25 (count parsed)))
-      (is (= 5 (count article)))
+    (let [stories (:stories (parse-feed feed))
+          article (first stories)]
+      (is (= 25 (count stories)))
       (is (= (:feed_type article) "RssFeed"))
       (is (= (:title article) "Opera zvyšuje stabilitu, oddělila pluginy do vlastních procesů"))
       (is (= (:url article) "http://www.root.cz/zpravicky/opera-zvysuje-stabilitu-oddelila-pluginy-do-vlastnich-procesu/#utm_source=rss&utm_medium=text&utm_campaign=rss"))
       (is (= (:description article) "Vyšla nová verze internetového prohlížeče Opera 12. Kromě nové podpory snadné změny vzhledu je zde i množství technických věcí, které nejsou na první pohled vidět. Například rychlejší start i s množstvím panelů, oddělení pluginů do samostatných..."))
       (is (= (:published_at article) #inst "2012-06-15T14:53:13.000-00:00")))))
+
+(deftest parse-feed-info-test
+  (let [feed (io/file rss-file)]
+    (is (instance? java.io.File feed))
+    (is (.exists feed))
+
+    (let [info (:info (parse-feed feed))]
+      (is (= (:url info) feed))
+      (is (= (:title info) "Root.cz - zprávičky"))
+      (is (= (:link info) "http://www.root.cz/zpravicky/"))
+      (is (= (:image info) "http://ii.iinfo.cz/r/rss-88x31.gif")))))
 
 (deftest add-story-test
   (let [story (Story. "test feed type" "test story title" "http://example.com/story" "test story description" #inst "2012-01-01T12:00")]
@@ -70,9 +80,8 @@
     (jdbc/with-connection @test-db-pool
       (jdbc/with-query-results res
         ["SELECT * FROM stories WHERE feed_id=? LIMIT 1" 12]
-        (is (= (first res)
-               {:id nil
-                :feed_id 12
+        (is (= (dissoc (first res) :id)
+               {:feed_id 12
                 :feed_type "test feed type"
                 :title "test story title"
                 :url "http://example.com/story"
@@ -85,9 +94,8 @@
     (jdbc/with-connection @test-db-pool
       (jdbc/with-query-results res
         ["SELECT * FROM rss_feeds WHERE link=? LIMIT 1" "http://testfeed.com"]
-        (is (= (first res)
-               {:id nil
-                :url "http://testfeed.com/feed.xml"
+        (is (= (dissoc (first res) :id)
+               {:url "http://testfeed.com/feed.xml"
                 :title "testfeed title"
                 :link "http://testfeed.com"
                 :image "http://testfeed.com/feed.png"} ))))))
@@ -101,3 +109,14 @@
                                           :id 87))
     (is (= 87 (:id (find-feed-by-url @test-db-pool "http://example.com/feed.xml"))))
     (is (= nil (find-feed-by-url @test-db-pool "http://nonexistent.com/feed.xml")))))
+
+(deftest find-story-by-url-test
+  (jdbc/with-connection @test-db-pool
+    (jdbc/insert-record :stories (assoc (Story. "test feed type"
+                                                "test story title"
+                                                "http://test-story.com/story"
+                                                "test story description"
+                                                #inst "2012-01-01T12:00")
+                                          :id 97))
+    (is (= 97 (:id (find-story-by-url @test-db-pool "http://test-story.com/story"))))
+    (is (= nil (find-story-by-url @test-db-pool "http://nonexistent.com/story")))))
