8fa230c22a1114dc419920ab34ba7762d7c4b7e7
mnicky <xmnicky@gmail.com>
2012-06-28 21:31:28 +0200

    implement -main function

--- project.clj
+++ project.clj
@@ -2,7 +2,7 @@
   :description "Backend for Sweeney."
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"
-            :comments "same as Clojure"}
+            :comments "the same as Clojure"}
   :dependencies [[org.clojure/clojure "1.4.0"]
                  [org.clojure/core.incubator "0.1.0"]
                  [overtone/at-at "1.0.0"]
@@ -13,6 +13,7 @@
                  [org.clojure/java.jdbc "0.2.3"]
                  [c3p0/c3p0 "0.9.1.2"]
                  [postgresql/postgresql "9.1-901.jdbc4"]]
+  :main sweeney-backend.core
   :profiles {:dev {:dependencies [[org.xerial/sqlite-jdbc "3.7.2"]]
                      ;:warn-on-reflection true
                      }}
--- src/clojure/sweeney_backend/core.clj
+++ src/clojure/sweeney_backend/core.clj
@@ -1,6 +1,18 @@
-(ns sweeney-backend.core)
+(ns sweeney-backend.core
+  (:gen-class)
+  (:require [sweeney-backend.actions :as actions]
+            [sweeney-backend.server :as server]))
+
+(defn bootstrap
+  "Adds default actions and runs the server."
+  []
+  (actions/add-action #{:check-feed} actions/check-feed-action "check feed")
+  (server/run)
+ )
 
 (defn -main
-  "I can even introduce myself ;-)"
+  "Main method that will start the backend."
   [& args]
-  (println "Hello, World! I'm Sweeney The Backend."))
+  (println "Starting Sweeney The Backend...")
+  (bootstrap)
+ )
