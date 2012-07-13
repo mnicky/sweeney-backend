3118733d85cc748d2ba29d35ca9d39347599bfe8
mnicky <xmnicky@gmail.com>
2012-07-13 13:40:07 +0200

    use new version of timbre which fixes the flush bug

--- project.clj
+++ project.clj
@@ -15,7 +15,7 @@
                  [org.slf4j/slf4j-simple "1.5.10"]
                  [postgresql/postgresql "9.1-901.jdbc4"]
                  [org.ccil.cowan.tagsoup/tagsoup "1.2.1"]
-                 [com.taoensso/timbre "0.6.1"]]
+                 [com.taoensso/timbre "0.7.0"]]
   :main sweeney-backend.core
   :profiles {:dev {:dependencies [[org.xerial/sqlite-jdbc "3.7.2"]]
                      ;:warn-on-reflection true
